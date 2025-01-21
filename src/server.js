d84d8f40b530515b83a5bdf020486141c157bcd9
Iury Escano <iurycordeiro15@gmail.com>
2025-01-21 18:17:21 -0300

    :bug: fix: Nodemon installed in project

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.get("/message/:id/:user", (req, res) => {
+  const { id, user } = req.params;
+  
+  res.send(`
+      Id da mensagem é: ${id}
+      Para o usuario: ${user}
+    `)
+});
+
+app.get("/users", (req, res) => {
+  const { page, limit } = req.query;
+
+  res.send(`Pagina: ${page} e Limite: ${limit}`);
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
