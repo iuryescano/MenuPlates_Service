2973eba739f01697e5eef635c1508e3093cb065b
Iury Escano <iurycordeiro15@gmail.com>
2025-01-22 15:16:16 -0300

    :bug: fix: express-async-erros installed in project

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError');
+
 class UsersController {
   create (request, response) {
     const { name, email, password } = request.body;
+
+    if(!name) {
+      throw new AppError('Name is required');
+    }
 
     //res.send(`User: ${name}, Mail: ${email}, Password: ${password}`); [TESTE]
     response.json({ name, email, password });
