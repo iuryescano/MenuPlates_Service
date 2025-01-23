97bfab10977e42cd378cbb646c3ad14a7fee6980
Iury Escano <iurycordeiro15@gmail.com>
2025-01-23 00:11:27 -0300

    :bug: fix: Created first migration in project

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ require("express-async-errors");
 
 const AppError = require('./utils/AppError');
 
+const migrationsRun = require('./database/sqlite/migrations');
+
 const express = require('express');
 
 const routes = require('./routes');
+migrationsRun();
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
 
 app.use(( error, request, response, next) => {
   if (error instanceof AppError) {
