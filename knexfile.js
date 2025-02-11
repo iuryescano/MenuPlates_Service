634a5baccaaee60e4a839704ea3449fe5d6bb9af
Iury Escano <iurycordeiro15@gmail.com>
2025-02-11 20:53:24 -0300

    :bug: fix: Corrigir conflitos de mesclagem no arquivo knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 const path = require("path");
-=======
-const path = require('path');
->>>>>>> a23cc67edbb73deac9a40e4a0ecafe1890e246fb
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-<<<<<<< HEAD
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
 
@@ -18,10 +13,4 @@ module.exports = {
 
     useNullAsDefault: true
   }
-=======
-      filename: path.resolve(__dirname, "src", "database", "database.db"),
-    },
-    useNullAsDefault: true,
-  },
->>>>>>> a23cc67edbb73deac9a40e4a0ecafe1890e246fb
 };
