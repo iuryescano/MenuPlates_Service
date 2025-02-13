2368c55772d9f51fce49136310145f0e8f54e848
Iury Escano <iurycordeiro15@gmail.com>
2025-02-13 13:42:42 -0300

    :bug: fix: JsonWebToken insert in project and token created in sessionsController

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
 const { compare } = require("bcryptjs");
 
 class SessionsController {
@@ -18,8 +21,13 @@ class SessionsController {
       throw new AppError("Email e/ou Senha Incorreta", 401);
     }
     
+    const { secret, expiresIn } = authConfig.jwt; //pegando o secret e o tempo de expiração do arquivo auth.js
+    const token = sign({}, secret, { //criando o token de fato
+      subject: String(user.id), //id do usuario
+      expiresIn //tempo de expiração do token
+    });
 
-    return response.json({ user });
+    return response.json({ user, token }); //retornando o usuario e o token
   }
 }
 
