b65d957edbf768c73cb62482171cf684e2f7817a
Iury Escano <iurycordeiro15@gmail.com>
2025-02-13 13:13:32 -0300

    started commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const platesRouter = require("./plates.routes");
 const ingredientsRouter = require("./ingredients.routes");
+const sessionsRouter = require("./sessions.routes");
 
 
 const routes = Router();
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/plates", platesRouter);
 routes.use("/ingredients", ingredientsRouter);
 
