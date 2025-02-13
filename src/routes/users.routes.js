19b04c8fd60fca07ec710615fc336390a7a32839
Iury Escano <iurycordeiro15@gmail.com>
2025-02-13 17:26:33 -0300

    :bug: fix: controller and middleware for users.Routes with token insert in project

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,22 +1,23 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRouter = Router();
 
-function myMiddleware(request, response, next) {
-  console.log("Voce passou pelo middleware") ;
+// function myMiddleware(request, response, next) {
+  //console.log("Voce passou pelo middleware") ;
 
  // if (!request.body.isAdmin) {
  //   return response.status(401).json({ error: "Usuario nao autenticado" });
  // } 
 
-  next();
-}
+ // next();
+//}
 
 const userController = new UsersController();
 
-usersRouter.post("/", myMiddleware, userController.create);
-usersRouter.put("/:id", userController.update);
+usersRouter.post("/", userController.create);
+usersRouter.put("/", ensureAuthenticated,  userController.update);
 
 module.exports = usersRouter; // Export app for testing
